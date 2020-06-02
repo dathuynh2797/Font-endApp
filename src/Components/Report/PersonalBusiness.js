a61574ef4f0ecc447101a67c2aec595a760f5bd0
Đạt Huỳnh <huynhtuandat2797@.com>
2020-06-02 16:48:31 +0700

    doanhso

--- src/Components/Report/PersonalBusiness.js
+++ src/Components/Report/PersonalBusiness.js
@@ -2,7 +2,7 @@
 import React, {Component, Fragment} from 'react';
 import SearchableDropdown from 'react-native-searchable-dropdown';
 import {LineChart, BarChart} from 'react-native-charts-wrapper';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, SafeAreaView, processColor} from 'react-native';
 
 var items = [
   {
@@ -38,30 +38,178 @@ var items = [
     name: 'Swift',
   },
 ];
+var year = [
+  {
+    id: 1,
+    name: '2019',
+  },
+  {
+    id: 2,
+    name: '2020',
+  },
+];
 export class PersonalBusiness extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      marker: {
+        enabled: true,
+        digits: 2,
+        backgroundTint: processColor('red'),
+        markerColor: processColor('#F0C0FF8C'),
+        textColor: processColor('white'),
+      },
+      legend: {
+        enabled: true,
+        textColor: processColor('red'),
+        textSize: 12,
+        form: 'SQUARE',
+        formSize: 14,
+        xEntrySpace: 10,
+        yEntrySpace: 5,
+        formToTextSpace: 5,
+        wordWrapEnabled: true,
+        maxSizePercent: 0.5,
+        custom: {
+          colors: [processColor('red'), processColor('red')],
+          labels: ['REFER', 'USER'],
+        },
+      },
+      xAxis: {
+        granularityEnabled: true,
+        granularity: 1,
+      },
+    };
   }
+
   render() {
     return (
-      <Fragment>
-        <SearchableDropdown />
+      <SafeAreaView style={{flex: 1, backgroundColor: 'grey'}}>
+        <View style={{flex: 2 / 3, backgroundColor: 'red'}}>
+          <SearchableDropdown
+            onTextChange={text => text}
+            onItemSelect={item => item}
+            items={items}
+            containerStyle={{marginTop: 20, marginHorizontal: 20}}
+            textInputStyle={{
+              padding: 10,
+              borderWidth: 1,
+              borderColor: '#ccc',
+              backgroundColor: '#FAF7F6',
+            }}
+            itemStyle={{
+              padding: 10,
+              marginTop: 4,
+              backgroundColor: '#FAF9F8',
+              borderColor: '#bbb',
+              borderWidth: 1,
+            }}
+            itemsContainerStyle={{
+              //items container style you can pass maxHeight
+              //to restrict the items dropdown hieght
+              maxHeight: 90,
+            }}
+          />
+          <View style={{flexDirection: 'row'}}>
+            <SearchableDropdown
+              onTextChange={text => text}
+              onItemSelect={item => item}
+              items={year}
+              containerStyle={{marginTop: 20, marginLeft: 20, flex: 1 / 3}}
+              textInputStyle={{
+                padding: 10,
+                borderWidth: 1,
+                borderColor: '#ccc',
+                backgroundColor: '#FAF7F6',
+              }}
+              itemStyle={{
+                padding: 10,
+                marginTop: 4,
+                backgroundColor: '#FAF9F8',
+                borderColor: '#bbb',
+                borderWidth: 1,
+              }}
+              itemsContainerStyle={{
+                //items container style you can pass maxHeight
+                //to restrict the items dropdown hieght
+                maxHeight: 90,
+              }}
+            />
+            <SearchableDropdown
+              onTextChange={text => text}
+              onItemSelect={item => item}
+              items={year}
+              containerStyle={{
+                marginTop: 20,
+                marginHorizontal: 5,
+                flex: 1 / 3,
+              }}
+              textInputStyle={{
+                padding: 10,
+                borderWidth: 1,
+                borderColor: '#ccc',
+                backgroundColor: '#FAF7F6',
+              }}
+              itemStyle={{
+                padding: 10,
+                marginTop: 4,
+                backgroundColor: '#FAF9F8',
+                borderColor: '#bbb',
+                borderWidth: 1,
+              }}
+              itemsContainerStyle={{
+                //items container style you can pass maxHeight
+                //to restrict the items dropdown hieght
+                maxHeight: 90,
+              }}
+            />
+            <SearchableDropdown
+              onTextChange={text => text}
+              onItemSelect={item => item}
+              items={year}
+              containerStyle={{marginTop: 20, marginRight: 20, flex: 1 / 3}}
+              textInputStyle={{
+                padding: 10,
+                borderWidth: 1,
+                borderColor: '#ccc',
+                backgroundColor: '#FAF7F6',
+              }}
+              itemStyle={{
+                padding: 10,
+                marginTop: 4,
+                backgroundColor: '#FAF9F8',
+                borderColor: '#bbb',
+                borderWidth: 1,
+              }}
+              itemsContainerStyle={{
+                //items container style you can pass maxHeight
+                //to restrict the items dropdown hieght
+                maxHeight: 90,
+              }}
+            />
+          </View>
+        </View>
         <View style={styles.container}>
           <LineChart
             style={styles.chart}
+            marker={this.state.marker}
             data={{
-              dataSets: [{label: 'demo', values: [{y: 1}, {y: 2}, {y: 1}]}],
+              dataSets: [
+                {
+                  label: 'demo',
+                  values: [{x: 1, y: 0}, {x: 2, y: 10}, {x: 10, y: 5}],
+                },
+              ],
             }}
           />
         </View>
-      </Fragment>
+      </SafeAreaView>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1 / 3,
     backgroundColor: '#F5FCFF',
   },
   chart: {
